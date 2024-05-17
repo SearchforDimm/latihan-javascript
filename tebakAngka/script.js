e9038f79652fdaa4fa9202fdffdf6c1ec908951d
Dimm <dmzrfqi@gmail.com>
2024-05-18 06:17:35 +0700

    tebakAngka new ver

--- tebakAngka/script.js
+++ tebakAngka/script.js
@@ -1,22 +1,43 @@
-var comp = Math.floor(Math.random() * 11);
+const comp = Math.floor(Math.random() * 11);
+
 console.log(comp);
+let lagi = true;
+let kesempatan = 0;
+let tebakan;
+while ( lagi ){
 
-var lagi = true;
-var selesai = false;
-while ( lagi === true ){
+    tebakan = window.prompt("Tebak Angka Diantara 1-10");
+    tebakan = Number(tebakan);
 
-var user = prompt("Tebak angka 1-10");
-alert("angka yang kamu masukan, " + user);
 
-    if (user < comp) {
-        alert("Kurang Tinggi");
-        lagi = confirm("Coba lagi");
-    } else if ( user > comp ) { 
-        alert("Lebih rendah");
-        lagi = confirm("Coba lagi")
+    if(isNaN(tebakan)) {
+        window.alert("tolong masukan angka.");
+    } else if (tebakan < 1 || tebakan > 10) {
+        window.alert("tolong masukan angka diantara 1-10");
     } else {
-        alert("Selamat, kamu benar");
-        lagi = false;
+        kesempatan++;
+        if(tebakan < comp) {
+            window.alert("terlalu rendah, coba lagi");
+        } else if(tebakan > comp) {
+            window.alert("terlalu tinggi, coba lagi");
+        } else {
+            window.alert(`selamat, kamu benar. kamu butuh ${kesempatan} kesempatan untuk menebak angkanya.`)
+            lagi = false;
+        }
     }
+
+// var user = prompt("Tebak angka 1-10");
+// alert("angka yang kamu masukan, " + user);
+
+//     if (user < comp) {
+//         alert("Kurang Tinggi");
+//         lagi = confirm("Coba lagi");
+//     } else if ( user > comp ) { 
+//         alert("Lebih rendah");
+//         lagi = confirm("Coba lagi")
+//     } else {
+//         alert("Selamat, kamu benar");
+//         lagi = false;
+//     }
 
 }
